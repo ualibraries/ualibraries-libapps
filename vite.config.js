eec173daccfb790f4ea060564113c76ff413b950
Ashley SlayMoore <aslay@arizona.edu>
2026-04-17 16:07:12 -0700

    Add script to remove Springshare styles, refs #17177

--- vite.config.js
+++ vite.config.js
@@ -38,12 +38,12 @@ export default defineConfig({
             const $ = cheerio.load(html);
 
             $("#header_ua, #header_site, #footer_site").remove();
-            $('link[href="/lookfeel.css"]').remove(); // Remove Springshare's Look & Feel CSS reference
-
+           
             $("body").prepend(`<div id="ualibraries-header"></div>`);
             $("body").append(`
               <div id="ualibraries-footer"></div>
               <script type="module" src="/helper.js"></script>
+              <script src="/src/main.js"></script>
             `);
 
             res.setHeader("Content-Type", "text/html");

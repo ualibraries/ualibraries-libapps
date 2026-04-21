bf8ce0f77858303769c7cf76ed43b7186cf9ba63
Rich Yu <zzcjbyrq@gmail.com>
2026-04-21 03:35:18 -0700

    Use TF to create a deployment user. Refs #17178

--- s3.tf
+++ s3.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "ual-terraform-state"
-    key            = "ualibraries-libapps"
+    bucket       = "ual-terraform-state"
+    key          = "ualibraries-libapps"
     use_lockfile = true
-    region         = "us-west-2"
+    region       = "us-west-2"
   }
   required_version = ">= 1.5.0"
 
@@ -39,8 +39,8 @@ resource "aws_s3_bucket_policy" "public_bucket" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid    = "PublicReadGetObject"
-        Effect = "Allow"
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
         Principal = "*"
         Action = [
           "s3:GetObject"
@@ -51,4 +51,47 @@ resource "aws_s3_bucket_policy" "public_bucket" {
   })
 
   depends_on = [aws_s3_bucket_public_access_block.public_bucket]
+}
+
+data "aws_iam_policy_document" "circleci_s3_policy_document" {
+  statement {
+    sid = "ListForTargetBucket"
+
+    actions = [
+      "s3:ListBucket"
+    ]
+
+    resources = [
+      aws_s3_bucket.public_bucket.arn
+    ]
+  }
+
+  statement {
+    sid = "ObjectOperationsForTargetBucket"
+
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.public_bucket.arn}/*"
+    ]
+  }
+}
+
+resource "aws_iam_user" "circleci" {
+  name = "ualibraries-libapps-circleci"
+}
+
+resource "aws_iam_policy" "circleci_s3_policy" {
+  name        = "ualibraries-libapps-circleci-policy"
+  description = "Allow CircleCI deployment access to the ualibraries libapps S3 bucket"
+  policy      = data.aws_iam_policy_document.circleci_s3_policy_document.json
+}
+
+resource "aws_iam_user_policy_attachment" "circleci_s3_policy_attachment" {
+  user       = aws_iam_user.circleci.name
+  policy_arn = aws_iam_policy.circleci_s3_policy.arn
 }

0d7be7d3b424ddadff9b5c2640f874ec8901b451
Andy Osborne <cao89@arizona.edu>
2026-06-03 17:10:48 -0700

    Add an HPS guide to the proxy list refs #17263

--- proxy-list.js
+++ proxy-list.js
@@ -12,6 +12,10 @@ export const proxyList = [
     target: "https://libguides.library.arizona.edu/library-guides",
   },
   {
+    prefix: "/lg-guide-hps",
+    target: "https://customertesting-ua.libguides.com/hps",
+  },
+  {
     prefix: "/lg-guide",
     target: "https://customertesting-ua.libguides.com/c.php?g=1531144&p=11471114&bs5=1",
   },

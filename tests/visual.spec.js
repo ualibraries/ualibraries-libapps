c2fb237ef5991f660d15416f2bf93703e0b4e52a
Andy Osborne <cao89@arizona.edu>
2026-07-08 11:49:11 -0700

    Update VR testing URL's to point at production refs #17260

--- tests/visual.spec.js
+++ tests/visual.spec.js
@@ -1,7 +1,7 @@
 import { expect, test } from "@playwright/test";
 
 test("A-Z Databases", async ({ page }) => {
-  await page.goto("https://customertesting-ua.libguides.com/az/databases");
+  await page.goto("https://libguides.library.arizona.edu/az/databases");
 
   await page.waitForLoadState("networkidle");
 
@@ -16,7 +16,7 @@ test("A-Z Databases filter", async ({ page }) => {
   await page.setViewportSize({ width: viewport.width, height: 1800 });
 
   await page.goto(
-    "https://customertesting-ua.libguides.com/az/databases?s=260432&t=64874",
+    "https://libguides.library.arizona.edu/az/databases?s=260432&t=64874",
   );
 
   await page.waitForLoadState("networkidle");

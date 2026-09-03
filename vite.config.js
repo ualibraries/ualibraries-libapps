c124c6d4f985429505e381a088df8bf3fa547e0f
Andy Osborne <cao89@arizona.edu>
2026-09-03 16:19:25 -0700

    Syntax cleanup refs #17457

--- vite.config.js
+++ vite.config.js
@@ -56,7 +56,7 @@ export default defineConfig({
       "/srch_process_cs.php": {
         target: "https://libguides.library.arizona.edu",
         changeOrigin: true,
-      }
+      },
     },
   },
   plugins: [
